2c1791720d42ec76bf35c314c735eee14307f315
Yvmartor <zavijana@gmail.com>
2020-12-09 15:47:49 +0100

    first push

---
+++ swingy/src/main/java/com/yvmartor/swingy/Swingy.java
@@ -0,0 +1,10 @@
+package com.yvmartor.swingy;
+
+import com.yvmartor.swingy.views.frames.MainFrame;
+
+public class Swingy {
+    public static void main(String[] args) {
+        MainFrame.buildFrame();
+        System.out.println("heho");
+    }
+}
---
+++ swingy/src/main/java/com/yvmartor/swingy/views/frames/MainFrame.java
@@ -0,0 +1,11 @@
+package com.yvmartor.swingy.views.frames;
+
+import javax.swing.*;
+
+public class MainFrame {
+
+    public static void buildFrame(){
+        JFrame myFrame = new JFrame();
+        myFrame.setVisible(true);
+    }
+}
