2c7a8c6c750abb14496185514d0ed51d4640d24a
Yvmartor <zavijana@gmail.com>
2021-01-21 14:35:29 +0100

    finish the map

--- swingy/src/main/java/com/yvmartor/swingy/views/gui/GUIAdventureView.java
+++ swingy/src/main/java/com/yvmartor/swingy/views/gui/GUIAdventureView.java
@@ -15,15 +15,19 @@ public class GUIAdventureView {
     }
     public void printGUIAdventureView(WorldMap worldMap, String[] options){
         int middle = (int)round((double)worldMap.getDimension()/2);
-        JPanel panel = new JPanel(new BorderLayout());
-        System.out.println(worldMap.getDimension());
+        JPanel panel = new JPanel();
         JLabel[][] map = new JLabel[worldMap.getDimension()][worldMap.getDimension()];
         for (int i = 0; i < worldMap.getDimension(); i++){
-            for (int y = 0; i < worldMap.getDimension(); y++){
-                if (i == middle && y == middle)
-                    map[i][y].setText("0");
-                else
-                    map[i][y].setText("X");
+            for (int y = 0; y < worldMap.getDimension(); y++){
+                if (i == middle && y == middle) {
+                    map[i][y] = new JLabel("O");
+                    map[i][y].setBackground(Color.BLACK);
+                }
+
+                else {
+                    map[i][y] = new JLabel("X");
+                    map[i][y].setBackground(Color.BLACK);
+                }
                 panel.add(map[i][y]);
             }
         }
